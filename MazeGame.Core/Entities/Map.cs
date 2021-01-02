6758960c944a93730c08359abf3767459351b457
DagothMor <siegfried1997@gmail.com>
2021-01-03 01:04:01 +0300

    -Added a simple main menu -Realize a Map class -Realize a display method

---
+++ MazeGame.Core/Entities/Map.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazeGame.Core.Entities
+{
+	public class Map
+	{
+		int[,] array = new int[2,2] { { 1,2},
+									  { 3,4} };
+
+		/// <summary>
+		/// Todo: Create a map generator
+		/// </summary>
+		public Map()
+		{
+			
+		}
+		public void Display()
+		{
+			for (int i = 0; i < this.array.GetLength(0); i++)
+			{
+				for (int j = 0; j < this.array.GetLength(1); j++)
+				{
+					Console.Write(array[i,j]);
+					Console.Write("|");
+				}
+				Console.WriteLine("");
+				Console.WriteLine("--------------");
+			}
+		}
+	}
+}
---
+++ MazeGame.Core/Program.cs
@@ -0,0 +1,46 @@
+﻿using MazeGame.Core.Entities;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MazeGame.Core
+{
+	class Program
+	{
+		/// <summary>
+		/// i'm gonna create a maze game which have a map redactor and algorithms to solve maps
+		/// well maybe the first i need a main menu
+		/// TODO: Create main menu,it has a exit/start game
+		/// </summary>
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.WriteLine("Welcome to the MazeGame!");
+				Console.WriteLine("Main menu:" +
+					"1-Start New Game!" +
+					"2-Exit");
+				var input = Console.ReadLine();
+				if (int.Parse(input) == 2)
+				{
+					Environment.Exit(0);
+				}//exit from game.
+				else if (int.Parse(input) == 1)//Start new game.
+				{
+					NewGame();
+				}
+				Console.Clear();
+			}
+		}
+		
+		/// <summary>
+		/// Need to create a matrix and displayed it
+		/// 1.Create a class Map
+		/// </summary>
+		public static void NewGame()
+		{
+			Map map = new Map();
+			map.Display();
+			Console.ReadLine();
+		}
+	}
+}
